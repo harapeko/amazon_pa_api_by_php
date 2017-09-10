ad27cae2dc14e23ea3c95fc85b494829994e5516
harapeko <tea.lile.tea@gmail.com>
2017-09-11 04:10:38 +0900

    fixed 環境変数を使うようにした

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('AWS_API_KEY',               getenv('AWS_API_KEY'));
+define('AWS_API_SECRET_KEY',        getenv('AWS_API_SECRET_KEY'));
+define('AWS_ASSOCIATE_TAG',         getenv('AWS_ASSOCIATE_TAG'));
+define('AWS_ANOTHER_ASSOCIATE_TAG', getenv('AWS_ANOTHER_ASSOCIATE_TAG'));
